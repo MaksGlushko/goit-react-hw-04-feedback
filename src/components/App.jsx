74d0323eaa924660bf0809c019fbf15d6282bb3b
MaksGlushko <133649841+MaksGlushko@users.noreply.github.com>
2023-12-14 15:15:06 +0100

    feedback 22

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import FeedbackOptions from './FeedBackOptions/FeedbackOptions';
+import { FeedbackOptions } from './FeedBackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
